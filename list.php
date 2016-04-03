05ed60afb16e735ffe5f2d4ffcee40857eda48e0
Szymon Skrzyński <skrzynski.szymon@gmail.com>
2016-04-03 22:11:25 +0200

    Now motivations

--- list.php
+++ list.php
@@ -43,5 +43,101 @@ return [
     [
         'Twoje życie staje się lepsze nie przez przypadek, ale dzięki zmianie.',
         'Jim Rohn'
-    ]
+    ],
+    [
+        'Do osiągnięcia kolejnego poziomu zawodowego i osobistego <br />niezbędny jest nowy wzorzec myślenia.',
+        'Anthony Robbins'
+    ],
+    [
+        'Liczy się nie tylko osiągnięcie celu, lecz także jakość życia,<br />której doświadczasz na drodze, która do celu prowadzi.',
+        'Anthony Robbins'
+    ],
+    [
+        'Jeśli chcesz wyleczyć nudę, rozwiń w sobie ciekawość.<br />Kiedy jesteś ciekawy, nic nie jest ani nudne, ani trudne.',
+        'Anthony Robbins'
+    ],
+    [
+        'Największym cierpieniem człowieka jest świadomość, <br />że żyje on poniżej poziomu własnych możliwości.',
+        'Anthony Robbins'
+    ],
+    [
+        'Tylko życie jest naprawdę ważne. Pamiętanie o tym, że kiedyś umrzesz jest najlepszym sposobem jaki znam na uniknięcie myślenia o tym, że masz cokolwiek do stracenia.',
+        'Steve Jobs'
+    ],
+    [
+        'Spędziłem większość życia, martwiąc się o rzeczy,<br />które nigdy się nie wydarzyły.',
+        'Mark Twain'
+    ],
+    [
+        'Trzymaj się z dala od ludzi, którzy próbują pomniejszać twoje ambicje. Mali ludzie zawsze tak robią, a naprawdę wielcy sprawiają, że czujesz, że i ty możesz być wielki.',
+        'Mark Twain'
+    ],
+    [
+        'Za dwadzieścia lat bardziej będziesz żałował tego, czego nie zrobiłeś, niż tego, co zrobiłeś. Więc odwiąż liny, opuść bezpieczną przystań. Złap w żagle pomyślne wiatry. Podróżuj, śnij, odkrywaj.',
+        'Mark Twain'
+    ],
+    [
+        'Wielkie umysły mają cele, inni mają życzenia.',
+        'Washington Irving'
+    ],
+    [
+        'Sposobem na zaczęcie jest skończenie mówienia<br />i podjęcie działania.',
+        'Walt Disney'
+    ],
+    [
+        'Ludzie, którzy nie mają problemów to ludzie martwi.<br />Dzień twojej śmierci będzie dniem bez problemów.',
+        'Nikodem Marszałek'
+    ],
+    [
+        'W życiu wielu ludzi wie, co robić, ale mało ludzi robi to, co wie…<br />Nie wystarczy tylko wiedzieć! Musisz podjąć działanie!',
+        'Anthony Robbins'
+    ],
+    [
+        'Gdybyśmy czekali na ten jeden moment, kiedy wszystko, absolutnie wszystko jest gotowe, nigdy byśmy nic nie zaczęli.',
+        'Iwan Turgieniew'
+    ],
+    [
+        'Jeśli nie chcesz być zapomnianym szybko po śmierci, <br/ >albo zaraz pisz rzeczy warte przeczytania, <br/ >albo czyń rzeczy warte opisania.',
+        'Benjamin Franklin'
+    ],
+    [
+        'Przyszłość zaczyna się dzisiaj, nie jutro.',
+        'Jan Paweł II'
+    ],
+    [
+        'Kto żyje nadzieją, umrze głodny.',
+        'Benjamin Franklin'
+    ],
+    [
+        'Nie starzeje się ten, kto nie ma na to czasu.',
+        'Benjamin Franklin'
+    ],
+    [
+        'Jesteś biedny i nieszczęśliwy nie dlatego, że Bóg tak chciał,<br />ale dlatego, że twój ojciec wpoił ci, że tak musi być.',
+        'Henry Ford'
+    ],
+    [
+        'Ludzie, którzy nie wierzą, nie wygrywają.',
+        'Włodzimierz Szaranowicz'
+    ],
+    [
+        'Jednym z największych odkryć jakie uczynił człowiek, i jedną z największych dla niego niespodzianek, jest odkrycie, że może on uczynić to, o czym ze strachem sądził, że nie potrafi uczynić.',
+        'Henry Ford'
+    ],
+    [
+        'Jeśli uważasz, że coś potrafisz lub że czegoś nie potrafisz,<br />na pewno masz rację.',
+        'Henry Ford'
+    ],
+    [
+        'Nic nie jest szczególnie trudne do zrobienia,<br />jeśli tylko rozłożyć to na etapy.',
+        'Henry Ford'
+    ],
+    [
+        'Z chwilą, gdy ktoś popadnie w stan umysłowy „rzeczoznawcy”,<br />wiele rzeczy staje się niemożliwe.',
+        'Henry Ford'
+    ],
+    [
+        'Jeśli śpisz więcej niż 6 godzin, to śpij szybciej.',
+        'Arnold Schwarzenegger'
+    ],
 ];
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
     $array = require_once '../list.php';
     $element = $array[array_rand($array)];
+//    $element = end($array);
 ?>
 <!doctype html>
 <html>
@@ -8,9 +9,18 @@
         <meta charset="UTF-8">
         <title>Zmotywuj się!</title>
 
-        <link href='https://fonts.googleapis.com/css?family=Roboto:600,500,400,300&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:600,500,400,300&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="/style.css">
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-75874290-1', 'auto');
+            ga('send', 'pageview');
+        </script>
     </head>
     <body>
         <div id="bg"></div>
@@ -21,8 +31,8 @@
             <div id="author"><?php echo $element[1]; ?></div>
         </div>
         <footer>
-            Autorem zdjęcia jest <a href="https://www.flickr.com/photos/75487768@N04/21479293212">barnyz</a>.
-            | W bazie jest <?php echo count($array); ?> cytatów. <a href="mailto:szym1991@gmail.com">Kontakt</a>
+            Autorem zdjęcia jest <a href="https://www.flickr.com/photos/q120/9692831755">Marie Sahlén</a>.
+            | Mamy <?php echo count($array); ?> cytatów. <a href="mailto:szym1991@gmail.com">Kontakt</a>
         </footer>
     </body>
 </html>
